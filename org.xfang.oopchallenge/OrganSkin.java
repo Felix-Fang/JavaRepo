718c50b7b7b7b4b6b0c3d71fef0987b1dc7ad44d
Felix-Fang <dhufangxing@hotmail.com>
2022-01-09 10:59:48 -0800

    remove unused files

--- org.xfang.oopchallenge/OrganSkin.java
+++
@@ -1,8 +0,0 @@
-package org.xfang.oopchallenge;
-
-public class OrganSkin extends Organ {
-    public OrganSkin(String name, String medicalCondition, String color) {
-        super(name, medicalCondition, color);
-    }
-
-}
