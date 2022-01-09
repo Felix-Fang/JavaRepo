4bd095fa625bae5f341a23659ca46043b3b868af
Felix-Fang <44922723+Felix-Fang@users.noreply.github.com>
2022-01-09 00:40:11 -0800

    Create OrganEye.java
    initial OrganEye

---
+++ org.xfang.oopchallenge/OrganEye.java
@@ -0,0 +1,42 @@
+package org.xfang.oopchallenge;
+
+public class OrganEye extends Organ {
+    private boolean isEyeClosed;
+
+    public OrganEye(String name, String medicalCondition, String color, boolean isEysClosed) {
+        super(name, medicalCondition, color);
+        this.isEyeClosed = isEysClosed;
+    }
+
+    public void eyeOperations() {
+        System.out.println("\t1. Closed the Eye");
+        System.out.println("\t2. Quit");
+
+    }
+
+    public boolean isEyeClosed() {
+        return isEyeClosed;
+    }
+
+    public void setEyeClosed(boolean eyeClosed) {
+        isEyeClosed = eyeClosed;
+    }
+
+    public void closeEye() {
+        if (isEyeClosed) {
+            System.out.println(this.name + " was closed");
+        } else {
+            isEyeClosed = true;
+            System.out.println(this.name + " is closed");
+        }
+    }
+
+    public void openEye() {
+        if (isEyeClosed) {
+            isEyeClosed = false;
+            System.out.println(this.name + " is open");
+        } else {
+            System.out.println(this.name + " was open");
+        }
+    }
+}
