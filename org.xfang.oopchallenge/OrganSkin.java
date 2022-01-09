ee3e0dd602b167fbbb8b24f8b33e8ac047325bdf
Felix-Fang <44922723+Felix-Fang@users.noreply.github.com>
2022-01-09 00:42:29 -0800

    Create OrganSkin.java
    initial OrganSkin

---
+++ org.xfang.oopchallenge/OrganSkin.java
@@ -0,0 +1,8 @@
+package org.xfang.oopchallenge;
+
+public class OrganSkin extends Organ {
+    public OrganSkin(String name, String medicalCondition, String color) {
+        super(name, medicalCondition, color);
+    }
+
+}
